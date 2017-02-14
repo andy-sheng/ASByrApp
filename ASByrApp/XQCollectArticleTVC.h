ead4a8ab5307d5a66b1c083eb3de12cf18e22e94
lixiangqian <1484937767@qq.com>
2017-02-14 14:47:32 +0800

    更改collection view为tableview

---
+++ ASByrApp/XQCollectArticleTVC.h
@@ -0,0 +1,13 @@
+//
+//  XQCollectArticleTVC.h
+//  ASByrApp
+//
+//  Created by lixiangqian on 17/2/14.
+//  Copyright © 2017年 andy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XQCollectArticleTVC : UITableViewController
+
+@end
